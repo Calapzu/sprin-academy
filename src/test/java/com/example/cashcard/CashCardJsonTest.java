7ca98b054f9b26607f9abdcae4a800faf8a4bcf2
Calapzu <yamsoncalapzu@gmail.com>
2023-07-24 20:02:45 -0500

    Se crea el proyecto siguiendo las clases de Spring Academy siguiendo la metodologia TDD

---
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package com.example.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+
+    @Test
+    public void myDirstTest(){
+        assertThat(1).isEqualTo(42);
+    }
+}
